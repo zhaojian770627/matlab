3bec2ac77eb1d32410a53191ee6f0ec51e91778f
zhaojian770627 <zhaojian770627@163.com>
2018-05-08 14:13:32 +0800

    add

---
+++ bisect.m
@@ -0,0 +1,23 @@
+%程序1.1 二分法
+%计算f(x)的近似解
+%输入:函数句柄f;a,b使得f(a)*f(b)<0,以及容差tol
+%输出:近似解
+function xc=bisect(f,a,b,tol)
+fa=f(a);
+fb=f(b);
+if(sign(fa)*sign(fb))>=0
+    error('f(a)f(b)<0 not satisfied!')
+end
+while(b-a)/2>tol
+    c=(a+b)/2;
+    fc=f(c);
+    if(fc==0)
+        break
+    end
+    if sign(fc)*sign(fa)<0 % a和c形成一个新的区间
+        b=c;fb=fc;
+    else
+        a=c;fa=fc;
+    end
+end
+xc=(a+b)/2;
