dddd7c6730b3bfc92538c18c5ed394a3c5605738
zhaojian770627 <zhaojian770627@163.com>
2018-06-07 22:56:57 +0800

    add

---
+++ broyden2.m
@@ -0,0 +1,13 @@
+% 输入: 初始向量x0,最大步数k
+% 输出: 解x
+% 使用实例:broyden2(f,[1:1],10)
+function x=broyden2(f,x0,k)
+[n,m]=sizeof(x0);
+b=eye(n,n);
+for i=1:k
+    x=x0-b*f(x0);
+    del=x-x0;
+    delta=f(x)-f(x0);
+    b=b+(del-b*delta)*del'*b/(del'*b*delta);
+    x0=x(k);
+end
