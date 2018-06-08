1353ae2cef89926ba6de0aca71b5f399f612b3fd
zhaojian770627 <zhaojian770627@163.com>
2018-06-08 22:14:36 +0800

    add

--- broyden2.m
+++ broyden2.m
@@ -2,12 +2,12 @@
 % 输出: 解x
 % 使用实例:broyden2(f,[1:1],10)
 function x=broyden2(f,x0,k)
-[n,m]=sizeof(x0);
+[n,m]=size(x0);
 b=eye(n,n);
 for i=1:k
     x=x0-b*f(x0);
     del=x-x0;
     delta=f(x)-f(x0);
     b=b+(del-b*delta)*del'*b/(del'*b*delta);
-    x0=x(k);
+    x0=x;
 end
