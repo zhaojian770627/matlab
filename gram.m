e4ba7c4021d551863a53b1ade840fb1c4720f1f5
zhaojian770627 <zhaojian770627@163.com>
2018-08-12 22:41:39 +0800

    add

---
+++ gram.m
@@ -0,0 +1,13 @@
+% 程序 格拉姆-施密特正交
+% A 向量矩阵
+% n 向量个数
+function [q,r]=gram(A,n)
+for j=1:n
+    y=A(:,j);
+    for i=1:j-1
+        r(i,j)=q(:,i)'*A(:,j);
+        y=y-r(i,j)*q(:,i);
+    end
+    r(j,j)=norm(y);
+    q(:,j)=y/r(j,j);
+end
