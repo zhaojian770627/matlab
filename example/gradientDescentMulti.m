82b67f8894e085c122bf2fbd6f2fe44caa5e45d0
zhaojianc <zhaojianc@yonyou.com>
2018-09-27 13:08:15 +0800

    add

--- example/gradientDescentMulti.m
+++ example/gradientDescentMulti.m
@@ -16,7 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    theta=theta-alpha*(sum((X*theta-y).*X)/m)';
+    
+    % theta=theta-alpha*(sum((X*theta-y).*X)/m)'; 下面的语句高效
+    theta = theta - alpha / m * X' * (X * theta - y); 
 
     % ============================================================
 
